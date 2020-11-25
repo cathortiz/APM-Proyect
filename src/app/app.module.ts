cca2661497ec98d93fb53aed7bb65c9df15bbb5f
catherin.ortiz <catherinortiz@gmail.com>
2020-11-25 01:41:53 -0300

    part 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule, FormsModule],
-  bootstrap: [AppComponent],
+  declarations: [
+  AppComponent, 
+  ProductListComponent, 
+  ConvertToSpacesPipe
+],
+  imports: [
+    BrowserModule, 
+    FormsModule],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
